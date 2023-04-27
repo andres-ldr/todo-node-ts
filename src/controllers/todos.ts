8a7931ffc2316526be331a3b58a2b5755df6ba06
Andrés <lopez.andresrigoberto@gmail.com>
2023-04-26 19:06:43 -0500

    [add] get todos controller

--- src/controllers/todos.ts
+++ src/controllers/todos.ts
@@ -1,17 +1,21 @@
 import { RequestHandler } from 'express';
 import { Todo } from '../models/todo';
 
-const TODO: Todo[] = [];
+const TODOS: Todo[] = [];
 
 export const createTodo: RequestHandler = (req, res, next) => {
   const text = (req.body as { text: string }).text;
 
   const newTodo = new Todo(Math.random().toString(), text);
 
-  TODO.push(newTodo);
+  TODOS.push(newTodo);
 
   res.status(201).json({
     message: 'Created the todo',
     createdTodo: newTodo,
   });
+};
+
+export const getTodos: RequestHandler = (req, res, next) => {
+  res.json({ todos: TODOS });
 };
